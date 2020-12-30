b2c70b2771328b2f321dc96cdc849c3316c9be76
Przemyslaw Malolepszy <przemyslaw.malolepszy@tomtom.com>
2020-12-30 22:06:57 +0100

    updated hints after review

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ let apiKey = "";
 let geofencingAdminKey = "";
 let geofencingProjectId = "";
 
-const circleCenterHint = "Click on a map to select center point.";
-const circleRadiusHint = "Click on a map to set circle radius.";
-const firstVertexHint = "Click on a map to select first vertex.";
-const rectSecondVertexHint = "Click on a map to select opposite vertex.";
-const polyNextVertexHint = "Click on a map to select next vertex. Double click to end drawing.";
-const corridorFirstPointHint = "Click on a map to select starting point.";
-const corridorNextPointHint = "Click on a map to select next point. Double click to end drawing.";
+const circleCenterHint = "Click on a map to select the center point.";
+const circleRadiusHint = "Click on a map to set the circle radius.";
+const firstVertexHint = "Click on a map to select the first vertex.";
+const rectSecondVertexHint = "Click on a map to select the opposite vertex.";
+const polyNextVertexHint = "Click on a map to select the next vertex. Double click it to end the drawing.";
+const corridorFirstPointHint = "Click on a map to select the starting point.";
+const corridorNextPointHint = "Click on a map to select the next point. Double click it to end the drawing.";
 
 function invalidJsonErrorMsg() { return `Error while parsing JSON properties.\nExample input:\n{"key": "abc",\n'key2': 2}`; }
 function saveFenceErrorMsg(err) { return `There was an error while saving the fence: ${err.response.data.message}`; }
